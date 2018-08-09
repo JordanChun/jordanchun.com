9031754d72d24804f1b8bb58521add144d0af494
Jordan <jordanchunxd@gmail.com>
2018-08-09 14:52:38 -0700

    update #2

--- js/web_dev_info.js
+++ js/web_dev_info.js
@@ -2,14 +2,14 @@ var projects = [
   {
     title: 'Ambrosia - Web Application',
     desc: 'Ambrosia is a resaurant web app that provides customers a dynamic cart system to order products. It also features an easy to use administration system to view, edit, and delete customer orders, inventory items, and add administrators. Coded with HTML, CSS, Javascript, jQuery, PHP, PHPTAL, MySQL',
-    img: 'img/ambrosia/ambrosia_logo.jpg',
+    img: 'img/ambrosia/ambrosia_logo.JPG',
     imgAlt: 'Ambrosia Logo',
     url: 'ambrosia.html'
   },
   {
     title: 'Pißwasser - Website',
     desc: 'A website redesign of a craft beer company. Users can browse through a section of craft beers or learn about the factory. Created with standard HTML, CSS, Javascript, jQuery, and PHP.',
-    img: 'img/piswasser/piswasser_01.jpg',
+    img: 'img/piswasser/piswasser_01.JPG',
     imgAlt: 'Pißwasser Home',
     url: 'piswasser.html'
   },
